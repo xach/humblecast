6fc73ca2a6361d2000a309339c9a20721f9d00f3
Zach Beane <xach@xach.com>
2015-08-21 20:59:03 -0400

    Drop s3 and move its only used function to humblecast.lisp

--- humblecast.lisp
+++ humblecast.lisp
@@ -4,6 +4,22 @@
 
 ;;; "humblecast" goes here. Hacks and glory await!
 
+(defun publish-cover-image (file)
+  (multiple-value-bind (width height)
+      (jpeg-dimensions file)
+    (unless (and width height)
+      (error "No dimensions found in ~A -- not a JPEG?" file))
+    (unless (and (<= 1400 height)
+                 (<= 1400 width))
+      (error "Image should be >1400px square"))
+    (zs3:put-file file (podcast-bucket) "cover.jpg"
+                  :public t
+                  :content-type "image/jpeg")
+    (values (zs3:resource-url :bucket (podcast-bucket)
+                              :key "cover.jpg"
+                              :vhost :cname)
+            width height)))
+
 (defclass humblecast (podcast)
   ((title
     :reader title
